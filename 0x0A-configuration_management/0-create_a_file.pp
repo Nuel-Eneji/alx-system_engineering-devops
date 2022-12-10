93b2866d3c750fd889f737f54f100caf640e92b1
Nuel-Eneji <enejiemmanueladie@gmail.com>
2022-12-10 14:32:04 -0800

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,11 @@
 # Create a file in '/tmp'
 
-file { 'holberton':
+file { 'school':
   ensure  => 'present',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
 }
 
